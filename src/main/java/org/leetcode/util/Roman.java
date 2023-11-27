9ff3d29dc9bf6d2e3ec4eb2a56267b2f946c99d7
Prashant <tanspra@gmail.com>
2023-11-27 10:05:21 +0530

    Problem 13 solved

---
+++ src/main/java/org/leetcode/util/Roman.java
@@ -0,0 +1,20 @@
+package org.leetcode.util;
+
+public enum Roman {
+    I("I", 1),
+    V("V", 5),
+    X("X", 10),
+    L("L", 50),
+    C("C", 100),
+    D("D", 500),
+    M("M", 1000);
+
+    private final String roman;
+    private final int val;
+
+    Roman(String roman, int val) {
+        this.roman = roman;
+        this.val = val;
+    }
+
+}
