633aa5cfcd358d68d0cb96dd7d950b089a4424be
Prashant <tanspra@gmail.com>
2023-12-11 20:16:15 +0530

    LongestPalindromicCombination -> completed
    Signed-off-by: Prashant <tanspra@gmail.com>

--- src/main/java/org/leetcode/LongestPalindromicCombination.java
+++ src/main/java/org/leetcode/LongestPalindromicCombination.java
@@ -4,40 +4,9 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
-/*https://leetcode.com/problems/longest-palindromic-substring/
-*
-* 5. Longest Palindromic Substring
-Medium
-Topics
-Companies
-Hint
-Given a string s, return the longest
-palindromic
-
-substring
- in s.
-
-
-
-Example 1:
-
-Input: s = "babad"
-Output: "bab"
-Explanation: "aba" is also a valid answer.
-Example 2:
-
-Input: s = "cbbd"
-Output: "bb"
-
-
-Constraints:
-
-1 <= s.length <= 1000
-s consist of only digits and English letters.
-* */
-public class LongestPalindromicSubstring {
+public class LongestPalindromicCombination {
     public static void main(String[] args) {
-        System.out.println(new LongestPalindromicSubstring().longestPalindrome("babad"));
+        System.out.println(new LongestPalindromicCombination().longestPalindrome("babad"));
     }
 
     public String longestPalindrome(String s) {
