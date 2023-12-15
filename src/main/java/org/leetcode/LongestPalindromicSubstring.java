475b641accae699a819bf08f18e3c99781e9511e
Prashant <tanspra@gmail.com>
2023-12-15 14:16:46 +0530

    Longest Palindrome -> solved

--- src/main/java/org/leetcode/LongestPalindromicSubstring.java
+++ src/main/java/org/leetcode/LongestPalindromicSubstring.java
@@ -33,45 +33,58 @@ s consist of only digits and English letters.
 * */
 public class LongestPalindromicSubstring {
     public static void main(String[] args) {
-        System.out.println(new LongestPalindromicSubstring().longestPalindrome("thelviymgkeddreyviespjsyqwmbmnlwzjhdokfzrczvreiagayofwvhecskjqlqzodtozvzozqyiwfsjyrinrmgfyhplybonzuvmxxyihmggwiuccplqjtgschmieoexvtewbsjqzkzapfxpzhgjtbmlchevohmxnbattphvobptnhmcoihcaimchurqpucxapojgszpopdvsfahwidiyxlpjfhdkcoewzvlmaebudtovnvcuadykhhmwfpilqfdvnseiitokcbuxmhwukrdxwvtgztczrwcsydqwosnktronibiplbljrcpinqorbhxrwjonnqeniebrksjkcmbvjnuwdedoenqmrcxayqbzmlpbubnfnkkqnuljtchaeijcmfpyuxkgfssoqliqmhowtbmcvzkqbanxhowjjejexxlihwwhilxxejejjwohxnabqkzvcmbtwohmqilqossfgkxuypfmcjieahctjlunqkknfnbubplmzbqyaxcrmqneodedwunjvbmckjskrbeineqnnojwrxhbroqnipcrjlblpibinortknsowqdyscwrzctzgtvwxdrkuwhmxubckotiiesnvdfqlipfwmhhkydaucvnvotdubeamlvzweockdhfjplxyidiwhafsvdpopzsgjopaxcupqruhcmiachiocmhntpbovhpttabnxmhovehclmbtjghzpxfpazkzqjsbwetvxeoeimhcsgtjqlpccuiwggmhiyxxmvuznobylphyfgmrniryjsfwiyqzozvzotdozqlqjkscehvwfoyagaiervzcrzfkodhjzwlnmbmwqysjpseivyerddekgmyivleht"));
+        System.out.println(new LongestPalindromicSubstring()
+                .longestPalindrome("thelviymgkeddreyviespjsyqwmbmnlwzjhdokfzrczvreiagayofwvhecskjqlqzodtozvzozqyiwfsjyrinrmgfyhplybonzuvmxxyihmggwiuccplqjtgschmieoexvtewbsjqzkzapfxpzhgjtbmlchevohmxnbattphvobptnhmcoihcaimchurqpucxapojgszpopdvsfahwidiyxlpjfhdkcoewzvlmaebudtovnvcuadykhhmwfpilqfdvnseiitokcbuxmhwukrdxwvtgztczrwcsydqwosnktronibiplbljrcpinqorbhxrwjonnqeniebrksjkcmbvjnuwdedoenqmrcxayqbzmlpbubnfnkkqnuljtchaeijcmfpyuxkgfssoqliqmhowtbmcvzkqbanxhowjjejexxlihwwhilxxejejjwohxnabqkzvcmbtwohmqilqossfgkxuypfmcjieahctjlunqkknfnbubplmzbqyaxcrmqneodedwunjvbmckjskrbeineqnnojwrxhbroqnipcrjlblpibinortknsowqdyscwrzctzgtvwxdrkuwhmxubckotiiesnvdfqlipfwmhhkydaucvnvotdubeamlvzweockdhfjplxyidiwhafsvdpopzsgjopaxcupqruhcmiachiocmhntpbovhpttabnxmhovehclmbtjghzpxfpazkzqjsbwetvxeoeimhcsgtjqlpccuiwggmhiyxxmvuznobylphyfgmrniryjsfwiyqzozvzotdozqlqjkscehvwfoyagaiervzcrzfkodhjzwlnmbmwqysjpseivyerddekgmyivleht"));
+    }
+
+    private static int expandAroundCenter(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        return right - left - 1;
     }
 
     public String longestPalindrome(String s) {
-        if (s.length() <= 1) {
-            return s;
+        if (s == null || s.length() == 0) {
+            return "";
         }
-        int maxLength = 1;
-        String maxStr = s.substring(0, 1);
-        int len = s.length();
-        for (int i = 0; i < s.length(); i++) {
-            for (int j = i + maxLength; j <= len; j++) {
-                if (j - i > maxLength && isPalindrome(s.substring(i, j))) {
-                    maxStr = s.substring(i, j);
-                    maxLength = j - i;
-                }
-            }
 
+        int start = 0;
+        int end = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
+            int maxLen = Math.max(len1, len2);
+
+            if (maxLen > end - start) {
+                start = i - (maxLen - 1) / 2;
+                end = i + maxLen / 2;
+            }
         }
-        return maxStr;
+
+        return s.substring(start, end + 1);
     }
 
-//    public String longestPalindrome(String s) {
-//        List<String> palindromeList = new ArrayList<>();
-//        int start = 0;
-//        int len = s.length();
-//        while (start <(len)) {
-//            for (int end = start + 1; end < len+1; end++) {
-//                if (isPalindrome(s.substring(start, end))) {
-//                    palindromeList.add(s.substring(start, end));
-//
-//                }
-//            }
-//            start++;
-//        }
-//        Optional result = palindromeList.stream().max(Comparator.comparing(String::length));
-//        if (result.isPresent()) return (String) result.get();
-//        else return "";
-//    }
+
+ /*   public String longestPalindrome(String s) {
+        List<String> palindromeList = new ArrayList<>();
+        int start = 0;
+        int len = s.length();
+        while (start <(len)) {
+            for (int end = start + 1; end < len+1; end++) {
+                if (isPalindrome(s.substring(start, end))) {
+                    palindromeList.add(s.substring(start, end));
+
+                }
+            }
+            start++;
+        }
+        Optional result = palindromeList.stream().max(Comparator.comparing(String::length));
+        if (result.isPresent()) return (String) result.get();
+        else return "";
+    }*/
 
 
     private boolean isPalindrome(String str) {
