ade7f360551996c69d10520df2c7dcfc6fa1a813
Prashant <tanspra@gmail.com>
2024-04-25 23:05:30 +0530

    MergeTwoSortedListUsingRecursion has been completed

--- src/main/java/org/leetcode/MergeTwoSortedListUsingRecursion.java
+++ src/main/java/org/leetcode/MergeTwoSortedListUsingRecursion.java
@@ -42,6 +42,17 @@ public class MergeTwoSortedListUsingRecursion {
     }
 
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+    if(list1 == null || list2 == null){
+        return (list1 != null) ? list1 : list2;
+    }
+    if(list2.val < list1.val ){
+        ListNode temp = list1;
+        list1 = list2;
+        list2 = temp;
 
+    }
+
+    list1.next = mergeTwoLists(list1.next,list2);
+    return list1;
     }
 }
