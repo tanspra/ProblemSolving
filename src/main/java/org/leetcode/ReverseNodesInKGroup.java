878817e1bdac31b5d269ccad8ba6be79065d4a6c
Prashant <tanspra@gmail.com>
2024-05-05 10:15:28 +0530

    SwapNodesInPairs has been completed

---
+++ src/main/java/org/leetcode/ReverseNodesInKGroup.java
@@ -0,0 +1,15 @@
+package org.leetcode;
+
+//https://leetcode.com/problems/reverse-nodes-in-k-group/description/
+public class ReverseNodesInKGroup {
+
+    public static void main(String[] args) {
+
+
+    }
+
+    public ListNode reverseKGroup(ListNode head, int k) {
+
+        return null;
+    }
+}
