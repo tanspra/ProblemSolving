9f28e241d18e795a18ee141b9ce6f7056b6d7c19
Prashant  Kumar Suman <tanspra@gmail.com>
2024-12-04 23:04:18 +0530

    FirstMissingPositive is completed and optimized.

--- src/main/java/org/leetcode/FirstMissingPositive.java
+++ src/main/java/org/leetcode/FirstMissingPositive.java
@@ -9,29 +9,52 @@ public class FirstMissingPositive {
 
     public int firstMissingPositive(int[] nums) {
         Arrays.sort(nums);
-        for (int i = 1; i <= nums.length+1; i++) {
-            if(!binarySearch(nums, i))
+        for (int i = 1; i <= nums.length + 1; i++) {
+            if (!binarySearch(nums, i))
                 return i;
         }
         return 1;
     }
 
     public boolean binarySearch(int[] nums, int target) {
-        int left =0;
-        int right = nums.length-1;
-        while(left <= right) {
-            int mid = left + (right - left)/2;
-            if(nums[mid] == target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
                 return true;
-            }
-            else if(nums[mid] > target) {
+            } else if (nums[mid] > target) {
                 right = mid - 1;
-            }
-
-            else if(nums[mid] < target) {
+            } else if (nums[mid] < target) {
                 left = mid + 1;
             }
         }
         return false;
     }
+
+    /*public int firstMissingPositiveOptimized(int[] nums) {
+        int n = nums.length;
+
+        // Step 1: Place each number in its correct position
+        for (int i = 0; i < n; i++) {
+            while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
+                // Swap nums[i] and nums[nums[i] - 1]
+                int temp = nums[nums[i] - 1];
+                nums[nums[i] - 1] = nums[i];
+                nums[i] = temp;
+            }
+        }
+
+        // Step 2: Find the first missing positive number
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+
+        // Step 3: If all numbers are in place, the missing number is n + 1
+        return n + 1;
+    }*/
 }
+
+
