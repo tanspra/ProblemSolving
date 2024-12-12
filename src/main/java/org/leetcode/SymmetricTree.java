b2b1dcaaf3c6db6f8de5880de9c867f1ed7c4d4f
Prashant  Kumar Suman <tanspra@gmail.com>
2024-12-12 21:36:35 +0530

    SymmetricTree is completed.

---
+++ src/main/java/org/leetcode/SymmetricTree.java
@@ -0,0 +1,26 @@
+package org.leetcode;
+
+import org.utility.TreeNode;
+
+public class SymmetricTree {
+    public static void main(String[] args) {
+        TreeNode treeNode = new TreeNode(1);
+        treeNode.left = new TreeNode(3);
+        treeNode.left.right = new TreeNode(2);
+        System.out.println(new SymmetricTree().isSymmetric(treeNode));
+    }
+
+    public boolean isSymmetric(TreeNode root) {
+        if(root == null) return true;
+        return isMirror(root.left, root.right);
+    }
+
+    private boolean isMirror(TreeNode left, TreeNode right) {
+        if(left == null && right == null) return true;
+
+        if(left == null || right == null)return false;
+
+        return (left.val == right.val) && isMirror(left.left, right.right) && isMirror(left.right, right.left);
+
+    }
+}
