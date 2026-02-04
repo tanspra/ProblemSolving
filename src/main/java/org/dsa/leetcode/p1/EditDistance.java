8834ebba878ef114abee2f28d366dbdb59ccd2d3
Prashant <tanspra@gmail.com>
2026-02-04 07:12:31 +0530

    Edit Distance improved.

--- src/main/java/org/dsa/leetcode/p1/EditDistance.java
+++ src/main/java/org/dsa/leetcode/p1/EditDistance.java
@@ -2,7 +2,7 @@ package org.dsa.leetcode.p1;
 
 public class EditDistance {
     public static void main(String[] args) {
-        System.out.println(new EditDistance().minDistance("zoologicoarchaeologist", "zoogeologist"));
+        System.out.println(new EditDistance().minDistance("abcd", "efg"));
     }
 
     public int minDistance(String word1, String word2) {
@@ -30,5 +30,32 @@ public class EditDistance {
             }
         }
        return dp[w2-1][w1-1];
+    }
+
+    public int minDistanceDp(String word1, String word2) {
+
+        //create dp
+        int m = word1.length();
+        int n = word2.length();
+        int[][] dp = new int[m+1][n+1];
+
+        //initially fill the dp
+        for(int i= 0; i <= m; i++){
+            dp[i][0] = i;
+        }
+        for(int i= 0; i <= n; i++){
+            dp[0][i] = i;
+        }
+        //now fill the rest dp
+        for(int i = 1; i <= m; i++){
+            for (int j = 1; j <= n ; j++) {
+                if(word1.charAt(m-i) == word2.charAt(n-j)){
+                    dp[i][j] = dp[i-1][j-1];
+                }else{
+                    dp[i][j] = Math.min(dp[i-1][j], Math.min(dp[i-1][j-1],dp[i][j-1])) + 1;
+                }
+            }
+        }
+        return dp[m][n];
     }
 }
