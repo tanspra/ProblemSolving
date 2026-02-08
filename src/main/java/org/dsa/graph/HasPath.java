257958e77e0445a32f36254cf2c40ef4f96875d7
Prashant <tanspra@gmail.com>
2026-02-09 05:26:09 +0530

    HasPath modified.

--- src/main/java/org/dsa/graph/HasPath.java
+++ src/main/java/org/dsa/graph/HasPath.java
@@ -2,20 +2,19 @@ package org.dsa.graph;
 
 public class HasPath {
     public static void main(String[] args) {
-        int[][] edges = {{0, 1, 0, 1},
-                {1, 0, 1, 1},
-                {0, 1, 1, 0},
-                {1, 1, 1, 0}};
-        System.out.println(hasPath(edges, 4, 0, 3, new int[4][4]));
+        int[][] edges = {{0, 1, 0, 0},
+                {1, 0, 1, 0},
+                {0, 1, 0, 0},
+                {0, 0, 0, 0}};
+        System.out.println(hasPath(edges, 4, 2, 0, new int[4]));
     }
 
-    private static boolean hasPath(int[][] edges, int n, int start, int end, int[][] visited) {
+    private static boolean hasPath(int[][] edges, int n, int start, int end, int[] visited) {
         if (start == end) return true;
         for (int i = 0; i < n; i++) {
-            if (edges[start][i] == 1 && visited[start][i] == 0) {
-                visited[start][i] = 1;
+            if (edges[start][i] == 1 && visited[i] == 0) {
+                visited[i] = 1;
                 if (hasPath(edges, n, i, end, visited)) return true;
-//                visited[start][i] = 0;
             }
         }
         return false;
