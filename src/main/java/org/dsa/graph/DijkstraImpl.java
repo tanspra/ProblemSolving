ce6d888db4afcfc48755f2403816c58e725ec6fd
Prashant <tanspra@gmail.com>
2026-02-12 07:24:28 +0530

    DijkstraImpl Completed.

---
+++ src/main/java/org/dsa/graph/DijkstraImpl.java
@@ -0,0 +1,48 @@
+package org.dsa.graph;
+
+public class DijkstraImpl {
+    public static void main(String[] args) {
+        int[][] graph = new int[][]{
+                {0, 1, 3, 0, 0},
+                {1, 0, 7, 2, 0},
+                {3, 7, 0, 5, 1},
+                {0, 2, 5, 0, 7},
+                {0, 0, 1, 7, 0}
+        };
+        System.out.println("Dijkstra's Algorithm");
+        dijkstra(graph, 5);
+    }
+
+    private static void dijkstra(int[][] graph, int n) {
+        boolean[] visited = new boolean[n];
+        int[] distance = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            visited[i] = false;
+            distance[i] = Integer.MAX_VALUE;
+        }
+        distance[2] = 0;
+        for (int i = 0; i < n - 1; i++) {
+            int minVertex = getMinVertex(visited, distance, n);
+            visited[minVertex] = true;
+            for (int j = 0; j < n; j++) {
+                if (!visited[j] && graph[minVertex][j] != 0 && distance[minVertex] + graph[minVertex][j] < distance[j]) {
+                    distance[j] = distance[minVertex] + graph[minVertex][j];
+                }
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.println(i + " " + distance[i]);
+        }
+    }
+
+    private static int getMinVertex(boolean[] visited, int[] distance, int n) {
+        int minVertex = -1;
+        for (int i = 0; i < n; i++) {
+            if (!visited[i] && (minVertex == -1 || distance[i] < distance[minVertex])) {
+                minVertex = i;
+            }
+        }
+        return minVertex;
+    }
+}
