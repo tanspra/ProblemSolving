9a8d41536148f403d40b7de36b7aab12aefaeab5
Prashant <tanspra@gmail.com>
2026-03-04 18:28:23 +0530

    CeilinaSortedArray, FindKthRotation, FindMinimumInRotatedSortedArray, KokoEatingBananas, Numberofoccurrence, PeakIndexinaMountainArray completed.

---
+++ src/main/java/org/dsa/leetcode/p5/KokoEatingBananas.java
@@ -0,0 +1,40 @@
+package org.dsa.leetcode.p5;
+
+public class KokoEatingBananas {
+    public static void main(String[] args) {
+        System.out.println(new KokoEatingBananas().minEatingSpeed(new int[]{805306368,805306368,805306368}, 1000000000));
+    }
+
+    public int minEatingSpeed(int[] piles, int h) {
+        int eatingSpeed = 1;
+        int low = 1, high = maxOfArray(piles);
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            long totalTimeTaken = findTotalTime(piles, mid);
+            if (totalTimeTaken > h) {
+                low = mid + 1;
+            } else {
+                eatingSpeed = mid;
+                high = mid - 1;
+            }
+        }
+    return eatingSpeed;
+    }
+
+    private long findTotalTime(int[] piles, int speed) {
+        long timeTaken = 0;
+        for (int pile : piles) {
+            timeTaken += pile / speed;
+            if (pile % speed != 0) timeTaken++;
+        }
+        return timeTaken;
+    }
+
+    private int maxOfArray(int[] piles) {
+        int max = piles[0];
+        for (int val : piles) {
+            if (val > max) max = val;
+        }
+        return max;
+    }
+}
