308e4bb190a063fdadfd73f1a839ad8f24a11912
Prashant <tanspra@gmail.com>
2026-03-07 13:28:23 +0530

    KthLargest, KthSmallest, KthSmallestElementInMultiplicationMatrix, KthSmallestElementinaSortedMatrix completed

---
+++ src/main/java/org/dsa/leetcode/p5/KthSmallestElementInMultiplicationMatrix.java
@@ -0,0 +1,29 @@
+package org.dsa.leetcode.p5;
+
+public class KthSmallestElementInMultiplicationMatrix {
+    public static void main(String[] args) {
+        System.out.println(new KthSmallestElementInMultiplicationMatrix().findKthNumber(3,3,5));
+    }
+    public int findKthNumber(int m, int n, int k) {
+      int low = 1, high = m*n;
+        while(low <= high){
+            int mid = low+(high-low)/2;
+            int count = findLessCount(m,n,mid);
+            if(count < k){
+                low = mid+1;
+            }else{
+                high = mid-1;
+            }
+        }
+        return low;
+    }
+
+    private int findLessCount(int m, int n, int mid) {
+        int count =0;
+        for(int i=1;i<=m;i++){
+            count += Math.min(n,mid/i);
+        }
+        return count;
+    }
+
+}
