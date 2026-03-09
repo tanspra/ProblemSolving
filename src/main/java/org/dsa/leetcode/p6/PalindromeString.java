d6cc23cb117b6aaaba32345b4b186100168ae08f
Prashant <tanspra@gmail.com>
2026-03-09 17:03:46 +0530

    PalindromeString, TaskScheduler completed.

---
+++ src/main/java/org/dsa/leetcode/p6/PalindromeString.java
@@ -0,0 +1,19 @@
+package org.dsa.leetcode.p6;
+
+public class PalindromeString {
+    public static void main(String[] args) {
+        System.out.println(new PalindromeString().isPalindrome("abba"));
+    }
+
+    boolean isPalindrome(String s) {
+        // code here
+        return palindromeHelper(s, 0, s.length()-1);
+    }
+    boolean palindromeHelper(String s, int low, int high){
+        int length = high - low + 1;
+        if(length == 0 || length ==1) return true;
+        if(s.charAt(low) != s.charAt(high))
+            return false;
+        return palindromeHelper(s, low+1, high-1);
+    }
+}
