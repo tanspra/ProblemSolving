66d80ad0097725d504f78ef883c1b01331a75de7
Prashant <tanspra@gmail.com>
2026-03-26 20:49:11 +0530

    LowestCommonAncesstorOfABinarySearchTree, SearchInABinarySearchTree completed.

---
+++ src/main/java/org/dsa/leetcode/tree/SearchInABinarySearchTree.java
@@ -0,0 +1,14 @@
+package org.dsa.leetcode.tree;
+
+public class SearchInABinarySearchTree {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null)
+            return null;
+        if (root.val == val)
+            return root;
+        else if (root.val < val)
+            return searchBST(root.right, val);
+        else
+            return searchBST(root.left, val);
+    }
+}
